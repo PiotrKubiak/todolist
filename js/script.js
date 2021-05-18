897272fb2448b76748f88f283aeabc01cb32ccc8
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-05-18 11:01:03 +0200

    added new style css

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
 {
-  const tasks = [                                                                       //Tablice obiektów. mamy 2 obiekty aby sprawdzić jak to będzie wyglądało (przykładowe)
-    {                                                                                   //Jeśli usuniemy będziemy mogli dodawać sobie jakąś własną
-      content: "zjeść śniadanie",
-      done: false,
-    },
-    {
-      content: "wstać rano",
-      done: true,
-    },
+  const tasks = [                                                                       //Tablice obiektów. możemy dodawać różne obiekty aby sprawdzić jak to będzie wyglądało (przykładowe)
+    //Jeśli usuniemy będziemy mogli dodawać sobie jakąś własną liste
+
   ];
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({                                                                       //dodaje nowe zadanie, przyjmuje 1 argument, przyjmuje treść (jest bez done)
+    tasks.push({                                                                        //dodaje nowe zadanie, przyjmuje 1 argument, przyjmuje treść (jest bez done)
       content: newTaskContent,
     });
-    render();                                                                         //żeby się coś wypisało trzeba znowu wywołać funkcję render
+    render();                                                                           //żeby się coś wypisało trzeba znowu wywołać funkcję render
   };
 
-  const removeTask = (taskIndex) => {                                                 //usuwa dane zadanie dostaje index zadania (taskIndex) i za pomocą metody splice usuwa je. 
+  const removeTask = (taskIndex) => {                                                   //usuwa dane zadanie dostaje index zadania (taskIndex) i za pomocą metody splice usuwa je. 
     tasks.splice(taskIndex, 1);
     render();
   }
 
-  const toggleTaskDone = (taskIndex) => {                                               //fukcja zmienia done w danym tasku, w tasku mamy taskIndex i teraz done i zaprzeczamy 
+  const toggleTaskDone = (taskIndex) => {                                                //fukcja zmienia done w danym tasku, w tasku mamy taskIndex i teraz done i zaprzeczamy 
     tasks[taskIndex].done = !tasks[taskIndex].done;                                      //teraz do done wpisujemy przeciwieństwo done
     render();
   }
@@ -38,7 +32,7 @@
 
     const toggleDoneButtons = document.querySelectorAll(".js-done");
 
-    toggleDoneButtons.forEach((toggleDoneButton, index) => {                                    //skreślamy zadania buttonem skreśl index
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {                            //skreślamy zadania buttonem skreśl index
       toggleDoneButton.addEventListener("click", () => {
         toggleTaskDone(index);
       })
@@ -50,11 +44,9 @@
                                                                                         //po każdej akcji od nowa robi całą zawartość listy, templet stings łączy się html z różnymi wyrażeniami
     for (const task of tasks) {
       htmlString += `                                                           
-        <li
-           ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-           >
-            <button class="js-done">zrobione?</button>
-            <button class="js-remove">usuń</button>              
+        <li class= "tasks__item${task.done ? " task__item tasks__item--done" : ""}">
+            <button class="tasks__button tasks__button--done js-done"> ok </button>
+            <button class="tasks__button tasks__button--remove js-remove"> U </button>              
            ${task.content}                                                              
         </li>
       `;                                                                                //dodawanie przekreślenia do zrobionych zadań
@@ -66,7 +58,7 @@
   };
 
   const onFormSubmit = (event) => {                                                   //blokada wysłania formularza, biore treść tego inputa 
-    event.preventDefault();                                   
+    event.preventDefault();
 
     const newTaskContent = document.querySelector(".js-newTask").value.trim();        //jeśli tekst pusty nic nie rób a jaśli coś wpisane dodaje nowe zadanie
 
