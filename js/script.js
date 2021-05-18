b33aed4b70a939bd4764311cd3331d088be0797b
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-05-18 06:46:08 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+{
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${task.content}
+                </li>
+                `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
