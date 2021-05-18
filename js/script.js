147532590501ed2ea9892084836e95f25c16a16d
Piotr Kubiak <kuberxxx1986@gmail.com>
2021-05-18 07:46:21 +0200

    adding new task and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,32 +1,57 @@
 {
-  const tasks = [                                                                 //tablica obiektów
-    {
-      content: "wstać rano",
-      done: true,
-    },
+  const tasks = [                                                                 
     {
       content: "zjeść śniadanie",
       done: false,
     },
+    {
+      content: "wstać rano",
+      done: true,
+    },
   ];
 
 
-  const render = () => {                                                        //renderowanie listy
+  const render = () => {                                                                //funkcja render na podstawie której wyrenderują się dane
     let htmlString = "";
 
-    for (const task of tasks) {                                                 //co robimy dla każdego zadania //konkatanacja  można przy każdym zadaniu coś dopisywać
+    for (const task of tasks) {                                                 
       htmlString += `                                                           
-        <li${task.done ? " style=\"text-decoration: line-trought\"" : ""}>
-           ${task.content}
+        <li
+           ${task.done ? " style=\"text-decoration: line-through\"" : ""}>              
+           ${task.content}                                                              
         </li>
-      `;
+      `;                                                                                //dodawanie przekreślenia do zrobionych zadań
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
-  const init = () => {
+  const addNewTask = (newTaskContent) => {
+    tasks.push({                                                                       //co robimy jeśli się coś wpisze
+      content: newTaskContent,
+    });
+
+    render();                                                                         //żeby się coś wypisało trzeba znowu wywołać funkcję render
+  };
+
+  const onFormSubmit = (event) => {                                                   //blokada wysłania formularza, biore treść tego inputa 
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();        //jeśli tekst pusty nic nie rób
+    
+    if(newTaskContent === "") {                                                       
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  }
+
+  const init = () => {                                                                //funkcja init przypisuje do form na submit funkcje onFormSubmit
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
